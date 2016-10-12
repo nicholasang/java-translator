41d4f192e6eedf573bf1eb6c603f643359eb8d62
AlexMarks <alm759@nyu.edu>
2016-10-12 18:50:25 -0400

    for testing dependency

--- src/test/java/inputs/testX/testDependency.java
+++ src/test/java/inputs/testX/testDependency.java
@@ -4,4 +4,13 @@ package inputs.testX;
  * Created by stud on 10/12/16.
  */
 public class testDependency {
+    public boolean testVariable = true;
+
+    public testDependency(){
+
+    }
+
+    public void testMethod(){
+        System.out.println("This does nothing.");
+    }
 }
--- src/test/java/inputs/testX/testMain.java
+++ src/test/java/inputs/testX/testMain.java
@@ -4,4 +4,7 @@ package inputs.testX;
  * Created by stud on 10/12/16.
  */
 public class testMain {
+    public static void main(String args[]){
+        testDependency X = new testDependency();
+    }
 }
