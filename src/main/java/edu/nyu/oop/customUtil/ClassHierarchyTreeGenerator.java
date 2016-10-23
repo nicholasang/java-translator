e35bc22362b43b11fbdc1304daf48aa45c9a0eef
KTRosenberg <ktr254@nyu.edu>
2016-10-23 12:13:47 -0400

    changed class names

---
+++ src/main/java/edu/nyu/oop/customUtil/ClassHierarchyTreeGenerator.java
@@ -0,0 +1,17 @@
+package edu.nyu.oop.customUtil;
+
+
+import xtc.tree.GNode;
+import java.util.*;
+
+public class ClassHierarchyTreeGenerator {
+    private ClassHierarchyTreeGenerator() {}
+
+    public static class ClassHierarchyTree {
+        // TODO:
+    }
+
+    public static ClassHierarchyTree create(ArrayList<GNode> allClasses) {
+        return new ClassHierarchyTree();
+    }
+}
