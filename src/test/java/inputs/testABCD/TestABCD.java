8cc8e40ebf25c1e6bc4ca7c9ca53b0c52c6c07e4
KTRosenberg <ktr254@nyu.edu>
2016-10-26 13:58:38 -0400

    mostly done, fixing one more bug

---
+++ src/test/java/inputs/testABCD/TestABCD.java
@@ -0,0 +1,50 @@
+package inputs.testABCD;
+
+
+public class TestABCD
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+
+
+class B extends A {
+    private String privateDataB = "dataB";
+
+    @Override
+    public String getArbitraryData() {
+        return this.privateDataB;
+    }
+
+}
+
+class C extends A {
+
+
+
+}
+
+class D {
+
+    void allByMyself() {
+        System.out.println("-don't wanna be all by myself anymore.");
+    }
+}
+
+class A {
+    public String publicDataA = "publicDataA";
+    protected String protectedDataA = "protectedDataA";
+    private String privateDataA = "privateDataA";
+    static String staticDataA = "staticDataA";
+
+    public String getArbitraryData() {
+        return this.publicDataA;
+    }
+
+    public static String[] Love() {
+        return new String[] {"is", "all", "you", "need"};
+    }
+
+}
