e02906584106e2703c342b5c9a19babee9c41627
KTRosenberg <ktr254@nyu.edu>
2016-10-25 23:10:49 -0400

    ABCD test classes created

---
+++ src/test/java/inputs/testABCDSimple/TestABCDSimple.java
@@ -0,0 +1,53 @@
+package inputs.testABCDSimple;
+
+
+public class TestABCDSimple
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
+
+class A {
+    public String publicDataA = "publicDataA";
+    protected String protectedDataA = "protectedDataA";
+    private String privateDataA = "privateDataA";
+    static String staticDataA = "staticDataA";
+
+    public String getArbitraryData()
+    {
+        return this.publicDataA;
+    }
+
+    public static String[] notSharingWithChildren()
+    {
+        return new String[]{"Ever", "ever", "EVER"};
+    }
+
+}
+
+class B extends A {
+    private String privateDataB = "dataB";
+
+    @Override
+    public String getArbitraryData()
+    {
+        return this.privateDataB;
+    }
+
+}
+
+class C extends A {
+
+
+
+}
+
+class D {
+
+    void allByMyself()
+    {
+        System.out.println("-don't wanna be all by myself anymore.");
+    }
+}
