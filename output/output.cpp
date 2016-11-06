1cbed49c508a4a0a83584cfec205243d75964764
AlexMarks <alm759@nyu.edu>
2016-11-06 14:14:01 -0500

    still need help to make this work :(

--- output/output.cpp
+++ output/output.cpp
@@ -1,1 +1,19 @@
-#include <io.stream> #pragma once namespace inputs {  namespace test001 {  class A{ public __String toString ()  { return "A" ; } } public class Test001{ public static void main (__String[] args )  {A a =  new  A (); std::cout << a.toString()<< endl ; } } }}
+#include "output.h"
+#include "java_lang.h"
+using namespace std;
+#include <iostream>
+namespace inputs { 
+ namespace test001 { 
+ class A{ 
+ public :  string toString () //__String not recognized
+ { return "A" ; 
+} 
+}; 
+class Test001{ 
+ int main () //main needs to be empty args and int
+ {A* a =  new  A (); //need *
+std::cout << a->toString()<< endl ; //use ->
+delete a; //need to add this for A*
+} 
+}; 
+}}
