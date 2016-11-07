b98e5d1ae6d3a5e45f9de447a5a25bd3b3440a28
KTRosenberg <ktr254@nyu.edu>
2016-11-06 20:00:28 -0500

    to avoid stashing

--- output/output.h
+++ output/output.h
@@ -5,42 +5,38 @@
 
 using namespace java::lang;
 
-namespace inputs
-{
-namespace test001
-{
+namespace inputs {
+namespace test001 {
 struct __A;
 struct __A_VT;
 typedef __A* A;
 
 
-struct __A
-{
-    __A_VT* __vptr;
-    static __A_VT __vtable;
+struct __A {
+__A_VT* __vptr;
+static __A_VT __vtable;
 
-    __A();
+__A();
 
-    static Class __class();
-    static String toString();
+static Class __class();
+static String toString();
 };
 
-struct __A_VT
-{
-    Class __isa;
-    int32_t (*hashCode) (__A);
-    bool (*equals) (__A, Object);
-    Class (*getClass) (__A);
-    String (*toString) (__A);
+struct __A_VT {
+Class __isa;
+int32_t (*hashCode) (__A);
+bool (*equals) (__A, Object);
+Class (*getClass) (__A);
+String (*toString) (__A);
 
-    __A_VT():
-        __isa((Class)__A::__class()),
-        hashCode((int32_t (*) (__A))&__Object::hashCode),
-        equals((bool (*) (__A, Object))&__Object::equals),
-        getClass((Class (*) (__A))&__Object::getClass),
-        toString((String (*) (__A))&__A::toString)
-    {
-    }
+__A_VT():
+__isa((Class)__A::__class()),
+ hashCode((int32_t (*) (__A))&__Object::hashCode),
+ equals((bool (*) (__A, Object))&__Object::equals),
+ getClass((Class (*) (__A))&__Object::getClass),
+ toString((String (*) (__A))&__A::toString)
+{
+}
 };
 
 }
