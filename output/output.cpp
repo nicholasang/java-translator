e6ba51c323e5c8dec6392a935fa9dc39d56f652c
KTRosenberg <ktr254@nyu.edu>
2016-12-18 00:38:30 -0500

    blah

--- output/output.cpp
+++ output/output.cpp
@@ -3,22 +3,29 @@
 using namespace std;
 #include <iostream>
 using namespace java::lang;
-namespace inputs { 
- namespace test002 { 
- 
+namespace inputs
+{
+namespace test002
+{
+
 
 __A_VT __A::__vtable;
 
-Class __A::__class() {
-static Class k = 
-new __Class(__rt::literal("class inputs.test002.A"), __Object::__class());
-return k;}
+Class __A::__class()
+{
+    static Class k =
+        new __Class(__rt::literal("class inputs.test002.A"), __Object::__class());
+    return k;
+}
 
 
 String __A::toString(A __this)
- {
- return (new __String("A")) ;
+{
+    return (new __String("A")) ;
 
 }
- __A::__A() :__vptr(&__vtable) {
-}}}
+__A::__A() :__vptr(&__vtable)
+{
+}
+}
+}
--- output/output.h
+++ output/output.h
@@ -5,40 +5,10 @@
 
 using namespace java::lang;
 
-namespace inputs {
-namespace test002 {
-struct __A;
-struct __A_VT;
-typedef __A* A;
-
-
-struct __A {
-__A_VT* __vptr;
-static __A_VT __vtable;
-
-__A();
-
-static Class __class();
-static String toString(A);
-static void __init(A);
-};
-
-struct __A_VT {
-Class __isa;
-int32_t (*hashCode) (A);
-bool (*equals) (A, Object);
-Class (*getClass) (A);
-String (*toString) (A);
-
-__A_VT():
-__isa((Class)__A::__class()),
- hashCode((int32_t (*) (A))&__Object::hashCode),
- equals((bool (*) (A, Object))&__Object::equals),
- getClass((Class (*) (A))&__Object::getClass),
- toString((String (*) (A))&__A::toString)
+namespace inputs
 {
-}
-};
+namespace test000
+{
 
 }
 }
